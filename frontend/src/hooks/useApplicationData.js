3c59ac1609484513a5a031e71bbd03bfbee4071f
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-08-02 16:19:17 -0400

    Implement useReducer to handle all the current states of the useApplicationData hook

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,32 +1,66 @@
-import {useState} from 'react';
+import {useReducer, useState} from 'react';
 
+const TOGGLE_FAV = 'TOGGLE_FAV';
+const OPEN_MODAL = 'OPEN_MODAL';
+const CLOSE_MODAL = 'CLOSE_MODAL';
+
+
+const reducer = (state, action) => {
+  console.log("THE action object: ", action);
+  switch(action.type) {
+    case TOGGLE_FAV:
+      if(state.favourites.includes(action.payLoad)) {
+        return {
+          ...state,
+          favourites: state.favourites.filter(item => item !== action.payLoad)
+        }
+      } else {
+        return {
+          ...state,
+          favourites: [...state.favourites, action.payLoad]
+        }
+      }
+    case OPEN_MODAL:
+      return {
+        ...state,
+        isModalOpen: true,
+        modalPhoto: action.payLoad
+      }
+    case CLOSE_MODAL:
+      return {
+        ...state,
+        isModalOpen: false,
+        modalPhoto: null
+      }
+    default:
+      throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
+  }
+};
+const initialState = {
+  favourites: [],
+  isModalOpen: false,
+  modalPhoto: null
+}
 const useApplicationData = () => {
-  const [favourites, setFavourites] = useState([]);
-  const [modal, setModal] = useState({isModalOpen: false, photo: null});
-  // const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleFav = (photo) => {
-    setFavourites(favourites => {
-      if(favourites.includes(photo)) {
-        return favourites.filter(item => item !== photo);
-      } else {
-        return [...favourites, photo];
-      }
-    });
+    dispatch({type: TOGGLE_FAV, payLoad: photo});
   };
   const openModal = (photo) => {
-    setModal({isModalOpen: true, photo});
+    dispatch({type: OPEN_MODAL, payLoad: photo});
   }
   const closeModal = () => {
-    setModal({isModalOpen: false, photo: null});
+    dispatch({type: CLOSE_MODAL});
   }
+
   return {
-    favourites,
+    favourites: state.favourites,
     toggleFav,
     openModal,
     closeModal,
-    selectedPhoto: modal.photo,
-    isModalOpen: modal.isModalOpen
+    selectedPhoto: state.modalPhoto,
+    isModalOpen: state.isModalOpen
   }
 };
 
