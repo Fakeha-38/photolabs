2b809799806de7e7183f5b9d8325451aee7bd948
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-08-07 13:34:06 -0400

    Fetch data from backend and then populate it in the app using useReduce states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.scss';
 import HomeRoute from './routes/HomeRoute';
 import PhotoDetailsModal from './routes/PhotoDetailsModal';
-import photos from './mocks/photos';
-import topics from './mocks/topics';
+// import photos from './mocks/photos';
+// import topics from './mocks/topics';
 import useApplicationData from './hooks/useApplicationData';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const {favourites, toggleFav, openModal, closeModal, selectedPhoto, isModalOpen} = useApplicationData();
+  const {favourites, toggleFav, openModal, closeModal, selectedPhoto, isModalOpen, photos, topics} = useApplicationData();
   
   return (
     <div className="App">
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,30 +1,30 @@
-import {useReducer, useState} from 'react';
+import {useReducer, useState, useEffect} from 'react';
 
 const TOGGLE_FAV = 'TOGGLE_FAV';
 const OPEN_MODAL = 'OPEN_MODAL';
 const CLOSE_MODAL = 'CLOSE_MODAL';
-
+const SET_PHOTOS = 'SET_PHOTOS';
+const SET_TOPICS = 'SET_TOPICS';
 
 const reducer = (state, action) => {
-  console.log("THE action object: ", action);
   switch(action.type) {
     case TOGGLE_FAV:
-      if(state.favourites.includes(action.payLoad)) {
+      if(state.favourites.includes(action.payload)) {
         return {
           ...state,
-          favourites: state.favourites.filter(item => item !== action.payLoad)
+          favourites: state.favourites.filter(item => item !== action.payload)
         }
       } else {
         return {
           ...state,
-          favourites: [...state.favourites, action.payLoad]
+          favourites: [...state.favourites, action.payload]
         }
       }
     case OPEN_MODAL:
       return {
         ...state,
         isModalOpen: true,
-        modalPhoto: action.payLoad
+        modalPhoto: action.payload
       }
     case CLOSE_MODAL:
       return {
@@ -32,29 +32,57 @@ const reducer = (state, action) => {
         isModalOpen: false,
         modalPhoto: null
       }
+    case SET_PHOTOS:
+      return {
+        ...state,
+        photoData: action.payload
+      }
+    case SET_TOPICS:
+      return {
+        ...state,
+        topicData: action.payload
+      }
     default:
       throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
   }
 };
 const initialState = {
+  photoData: [],
+  topicData: [],
   favourites: [],
   isModalOpen: false,
   modalPhoto: null
 }
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    // use the fetch API to make an AJAX request to the backend
+    fetch('http://localhost:8001/api/photos')
+      .then(res => res.json())
+      .then(data => {
+        dispatch({type: SET_PHOTOS, payload: data})
+      });
+    fetch('http://localhost:8001/api/topics')
+      .then(res => res.json())
+      .then(data => {
+        dispatch({type: SET_TOPICS, payload: data})
+      });
+
+  }, []);
 
   const toggleFav = (photo) => {
-    dispatch({type: TOGGLE_FAV, payLoad: photo});
+    dispatch({type: TOGGLE_FAV, payload: photo});
   };
   const openModal = (photo) => {
-    dispatch({type: OPEN_MODAL, payLoad: photo});
+    dispatch({type: OPEN_MODAL, payload: photo});
   }
   const closeModal = () => {
-    dispatch({type: CLOSE_MODAL});
+    dispatch({type: CLOSE_MODAL, payload: null});
   }
-
+  // console.log("State photos data/Array: ", state.photoData);
   return {
+    topics: state.topicData,
+    photos: state.photoData,
     favourites: state.favourites,
     toggleFav,
     openModal,
